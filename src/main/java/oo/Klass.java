932aff8796b8e95b7f0c1f5940d27d826c7c2da4
Daniel Siu <daniel.siu@oocl.com>
2024-11-21 14:26:43 +0800

    feat: add Klass object and add to Student class

--- src/main/java/oo/Klass.java
+++ src/main/java/oo/Klass.java
@@ -1,4 +1,30 @@
 package oo;
 
+import java.util.Objects;
+
 public class Klass {
+
+    private Integer number;
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public Klass(Integer number) {
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Klass klass = (Klass) o;
+        return Objects.equals(number, klass.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
+    }
+
 }
--- src/main/java/oo/Student.java
+++ src/main/java/oo/Student.java
@@ -4,8 +4,27 @@ public class Student extends Person{
 
     private static final String JOB = "student";
 
+    private Klass klass;
+
     public Student(Integer id, String name, Integer age) {
         super(id, name, age, JOB);
+    }
+
+    public void join(Klass klass) {
+        this.klass = klass;
+    }
+
+    public boolean isIn(Klass klass) {
+        return this.klass.equals(klass);
+    }
+
+    public Student(Integer id, String name, Integer age, String job) {
+        super(id, name, age, job);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a %s. I am in class %d", name, age, job, klass.getNumber());
     }
 
 }
