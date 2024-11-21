721e4de9228bea19d7c185ffce5c1e065b02faeb
Daniel Siu <daniel.siu@oocl.com>
2024-11-21 13:57:42 +0800

    feat: implement Person class

--- src/main/java/oo/Person.java
+++ src/main/java/oo/Person.java
@@ -1,4 +1,41 @@
 package oo;
 
+import java.util.Objects;
+
 public class Person {
+
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    public Person(Integer id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old.", name, age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object anotherPerson) {
+        if (anotherPerson == null) {
+            return false;
+        }
+        if (anotherPerson instanceof Person) {
+            return this.id.equals(((Person) anotherPerson).id);
+        } else {
+            return false;
+        }
+
+    }
+
 }
