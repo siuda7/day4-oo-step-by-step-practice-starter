8e8d3a10a4147f402d13ec95e869a377775e85c6
Daniel Siu <daniel.siu@oocl.com>
2024-11-21 14:08:44 +0800

    test: add should_return_message_with_name_and_age_when_introduce_given_student

--- src/test/java/oo/step2/StudentTest.java
+++ src/test/java/oo/step2/StudentTest.java
@@ -7,13 +7,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class StudentTest {
 
-//    @Test
-//    public void should_return_message_with_name_and_age_when_introduce_given_student() {
-//        Student tom = new Student(1, "Tom", 18);
-//        String introduce = tom.introduce();
-//
-//        assertThat(introduce).isEqualTo("My name is Tom. I am 18 years old. I am a student.");
-//    }
+    @Test
+    public void should_return_message_with_name_and_age_when_introduce_given_student() {
+        Student tom = new Student(1, "Tom", 18);
+        String introduce = tom.introduce();
+
+        assertThat(introduce).isEqualTo("My name is Tom. I am 18 years old. I am a student.");
+    }
 //
 //    @Test
 //    public void should_return_message_with_name_and_age_when_introduce_given_another_student() {
