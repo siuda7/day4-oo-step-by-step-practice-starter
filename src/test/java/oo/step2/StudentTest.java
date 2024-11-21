31399eff9564870c79306591452f9e58048601ed
Daniel Siu <daniel.siu@oocl.com>
2024-11-21 14:09:59 +0800

    test: add should_return_false_when_equals_given_two_students_with_different_id

--- src/test/java/oo/step2/StudentTest.java
+++ src/test/java/oo/step2/StudentTest.java
@@ -23,19 +23,19 @@ public class StudentTest {
         assertThat(introduce).isEqualTo("My name is Jim. I am 19 years old. I am a student.");
     }
 
-//    @Test
-//    public void should_return_true_when_equals_given_two_students_with_same_id() {
-//        Student student1 = new Student(1, "Tom", 18);
-//        Student student2 = new Student(1, "Tommy", 19);
-//
-//        assertThat(student1).isEqualTo(student2);
-//    }
-//
-//    @Test
-//    public void should_return_false_when_equals_given_two_students_with_different_id() {
-//        Student student1 = new Student(1, "Tom", 18);
-//        Student student2 = new Student(2, "Tom", 18);
-//
-//        assertThat(student1).isNotEqualTo(student2);
-//    }
+    @Test
+    public void should_return_true_when_equals_given_two_students_with_same_id() {
+        Student student1 = new Student(1, "Tom", 18);
+        Student student2 = new Student(1, "Tommy", 19);
+
+        assertThat(student1).isEqualTo(student2);
+    }
+
+    @Test
+    public void should_return_false_when_equals_given_two_students_with_different_id() {
+        Student student1 = new Student(1, "Tom", 18);
+        Student student2 = new Student(2, "Tom", 18);
+
+        assertThat(student1).isNotEqualTo(student2);
+    }
 }
