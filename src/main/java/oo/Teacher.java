f716b53b17b47f2d3e50c655d5e8d25649df8d4f
Daniel Siu <daniel.siu@oocl.com>
2024-11-21 14:06:42 +0800

    feat: add Teacher class

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,4 +1,13 @@
 package oo;
 
-public class Teacher {
+public class Teacher extends Person{
+
+    public Teacher(Integer id, String name, Integer age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a teacher.", name, age);
+    }
 }
