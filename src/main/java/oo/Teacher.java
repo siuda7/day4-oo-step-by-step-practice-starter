9fe6f06d9d3f27e22a6d8a3b333d26a4a7af6be5
Daniel Siu <daniel.siu@oocl.com>
2024-11-21 14:52:01 +0800

    feat: add isTeaching belongsTo assignTo method and update introduce method in teacher class

--- src/main/java/oo/Teacher.java
+++ src/main/java/oo/Teacher.java
@@ -1,10 +1,40 @@
 package oo;
 
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toList;
+
 public class Teacher extends Person{
 
     private static final String JOB = "teacher";
 
+    private ArrayList<Klass> klasses = new ArrayList<>();
+
     public Teacher(Integer id, String name, Integer age) {
         super(id, name, age, JOB);
+    }
+
+    public void assignTo(Klass klass) {
+        klasses.add(klass);
+    }
+
+    public boolean belongsTo(Klass klass) {
+        return klasses.contains(klass);
+    }
+
+    private String getKlasses() {
+        return klasses.stream()
+                .map(klass -> klass.getNumber().toString())
+                .collect(Collectors.joining(", "));
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a teacher. I teach Class %s.",name, age, getKlasses());
+    }
+
+    public boolean isTeaching(Student student) {
+        return klasses.contains(student.getKlass());
     }
 }
