994974b82531c5a04c9e85e76c39c10d46b46aa0
Daniel Siu <daniel.siu@oocl.com>
2024-11-21 16:13:34 +0800

    test: add should_return_false_when_isLeader_given_student_is_in_the_class_but_not_the_leader to KlassTest

--- src/test/java/oo/step5/KlassTest.java
+++ src/test/java/oo/step5/KlassTest.java
@@ -26,41 +26,41 @@ public class KlassTest {
 
         assertThat(klass.isLeader(tom)).isFalse();
     }
-//
-//    @Test
-//    public void should_return_false_when_isLeader_given_student_is_in_the_class_but_not_the_leader() {
-//        Student tom = new Student(1, "Tom", 21);
-//        Klass klass = new Klass(1);
-//        tom.join(klass);
-//
-//        boolean isLeader = klass.isLeader(tom);
-//
-//        assertThat(isLeader).isFalse();
-//    }
-//
-//    @Test
-//    public void should_return_true_when_isLeader_given_student_is_the_leader() {
-//        Student tom = new Student(1, "Tom", 21);
-//        Klass klass = new Klass(1);
-//        tom.join(klass);
-//        klass.assignLeader(tom);
-//
-//        boolean isLeader = klass.isLeader(tom);
-//
-//        assertThat(isLeader).isTrue();
-//    }
-//
-//    @Test
-//    public void should_print_prompt_message_when_assign_leader_given_student_is_not_in_class() throws Exception {
-//        Student tom = new Student(1, "Tom", 21);
-//        Klass klass = new Klass(1);
-//
-//        klass.assignLeader(tom);
-//
-//        assertThat(systemOut()).contains("It is not one of us.");
-//    }
-//
-//    private String systemOut() {
-//        return outContent.toString();
-//    }
+
+    @Test
+    public void should_return_false_when_isLeader_given_student_is_in_the_class_but_not_the_leader() {
+        Student tom = new Student(1, "Tom", 21);
+        Klass klass = new Klass(1);
+        tom.join(klass);
+
+        boolean isLeader = klass.isLeader(tom);
+
+        assertThat(isLeader).isFalse();
+    }
+
+    @Test
+    public void should_return_true_when_isLeader_given_student_is_the_leader() {
+        Student tom = new Student(1, "Tom", 21);
+        Klass klass = new Klass(1);
+        tom.join(klass);
+        klass.assignLeader(tom);
+
+        boolean isLeader = klass.isLeader(tom);
+
+        assertThat(isLeader).isTrue();
+    }
+
+    @Test
+    public void should_print_prompt_message_when_assign_leader_given_student_is_not_in_class() throws Exception {
+        Student tom = new Student(1, "Tom", 21);
+        Klass klass = new Klass(1);
+
+        klass.assignLeader(tom);
+
+        assertThat(systemOut()).contains("It is not one of us.");
+    }
+
+    private String systemOut() {
+        return outContent.toString();
+    }
 }
